b942a6ec20fa723722ddaf35702d405c96d35425
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-10 11:48:29 +0600

    comment added

--- N._Char.c
+++ N._Char.c
@@ -5,7 +5,7 @@ int main()
     scanf("%c",&ch);
     int A = ch; //taking ASCII Value from the corresponding character
     // printf("%d\n",A); //Checking
-    // if('A'<=A && A<='Z')// ei conditon ke evabe likhleo cholbe
+    // if('A'<=A && A<='Z')//The condition should also work if we write this way.
     if(65<=A && A<=90){
         int B = A + 32;
         // printf("%d\n",B); //Checking
