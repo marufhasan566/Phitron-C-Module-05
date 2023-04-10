dfd3fc89efd79836906e955b7a8f14fa9594f6c2
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-10 12:35:48 +0600

    first digit added

---
+++ P._First_digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    scanf("%d",&X);
+    int N=X/1000;
+    printf("%d\n",N);
+    if(N%2==0){
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+    return 0;
+}
