e218b88ae89cd2c2f185277a070899091a78210f
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-10 11:45:12 +0600

    N. Char added

--- N._Char.c
+++ N._Char.c
@@ -1,13 +1,25 @@
-// #include<stdio.h>
-// int main()
-// {
-    
-//     return 0;
-// }
 #include <stdio.h>
 int main()
 { 
-    char A=A;
-    printf("%d",A);
+    char ch;
+    scanf("%c",&ch);
+    int A = ch; //taking ASCII Value from the corresponding character
+    // printf("%d\n",A); //Checking
+    // if('A'<=A && A<='Z')// ei conditon ke evabe likhleo cholbe
+    if(65<=A && A<=90){
+        int B = A + 32;
+        // printf("%d\n",B); //Checking
+        printf("%c\n",B);
+    }
+    // else if(97<=A && A<=122){
+    //     int B = A - 32;
+    //     printf("%d\n",B); //Checking
+    //     printf("%c\n",B);
+    // }
+    else{
+        int B = A - 32;
+        // printf("%d\n",B); //Checking
+        printf("%c\n",B);
+    }
     return 0;
 }
