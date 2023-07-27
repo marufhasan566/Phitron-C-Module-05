0c9c8319f8267c208cf2b10b9bba807be8dc4229
marufhasan566 <marufhasan.mh1@gmail.com>
2023-07-27 17:28:02 +0600

    small correction

--- M._Capital_or_Small_or_Digit.c
+++ M._Capital_or_Small_or_Digit.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int X;
-    scanf("%c",&X);
+    char X;
+    scanf("%c", &X);
     int N = X;
-    // if(N>='0' && N<='9') //the following can also be written like this 
-    if(N>=48 && N<=57){    
+    // if(N>='0' && N<='9') //the following can also be written like this
+    if (N >= 48 && N <= 57)
+    {
         printf("IS DIGIT");
     }
-    else if(N>=65 && N<=90){
+    else if (N >= 65 && N <= 90)
+    {
         printf("ALPHA\nIS CAPITAL");
     }
-    else if(N>=97 && N<=122){
+    else if (N >= 97 && N <= 122)
+    {
         printf("ALPHA\nIS SMALL");
     }
     return 0;
